6bb18d6ec433b9ca219f67e0111d4badf8b6f063
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-04-30 15:03:36 -0300

    Create ex01.cpp

---
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/LinguagemC --- Daniel-Miguel/ATV-CPP-001-lista-execicios.md/ex01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int primo,i;
+	bool vprimo = true;
+		
+	cout << "digite um numero inteiro: ";
+	cin >> primo;
+	
+	if (primo==0 or primo==1){
+		cout << "o numero não é primo" ;
+	return 0;
+	}
+
+
+for (i = 2; i*i <= primo; i++){
+	if (primo % i == 0){		
+		vprimo = false;
+		break;
+	}else{
+		vprimo = true;
+	}
+}
+	
+	
+if(vprimo == false ){
+  cout << "seu numero não é primo";
+}else{
+	cout << "seu numero é primo";
+}
+
+return 0;
+}
