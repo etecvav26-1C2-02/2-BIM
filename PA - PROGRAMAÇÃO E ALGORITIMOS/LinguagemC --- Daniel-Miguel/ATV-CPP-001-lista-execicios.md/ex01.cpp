48659088fec3720231d35c333afc7b50e90a8c50
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-05-07 19:44:53 -0300

    Update ex01.cpp

--- PA - PROGRAMAÇÃO E ALGORITIMOS/LinguagemC --- Daniel-Miguel/ATV-CPP-001-lista-execicios.md/ex01.cpp
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/LinguagemC --- Daniel-Miguel/ATV-CPP-001-lista-execicios.md/ex01.cpp
@@ -1,3 +1,8 @@
+/*
+   Autores: Daniel de Sousa,Miguwl Marcelo
+   Data: 04/05/2026
+   Finalidade: Verificar se um número inteiro é primo contando a quantidade de divisores.
+*/
 #include <iostream>
 using namespace std;
 
