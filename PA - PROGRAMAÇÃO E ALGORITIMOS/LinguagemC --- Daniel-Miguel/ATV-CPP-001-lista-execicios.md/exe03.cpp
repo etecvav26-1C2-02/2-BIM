0fa334a213b04f6feb743b9ba649c286aca3dd72
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-05-07 14:18:05 -0300

    Create exe03.cpp

---
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/LinguagemC --- Daniel-Miguel/ATV-CPP-001-lista-execicios.md/exe03.cpp
@@ -0,0 +1,24 @@
+
+ /* Ler vários números até que seja digitado um número negativo. Calcular e exibir a soma desses números./*
+
+using namespace std;
+#include <iostream>
+
+int main() {
+    int numero, soma = 0 ;
+    
+    do {
+        cout << "escreva o numero: ";
+        cin >> numero;
+
+        if (numero >= 0) {
+          
+            soma = soma + numero;
+        }
+        
+ } while (numero >= 0);
+      
+ cout << "A soma é:  " << soma << endl;
+ 
+    return 0;
+}
