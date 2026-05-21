9a8cacfedc31ec30c7749b805ea64ec554bc063f
Cauã Balzaneli <cauabalzaneli81@gmail.com>
2026-05-21 20:46:33 -0300

    add ex15

---
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/LinguagemC --- Valentino-Caua/L2/ex15/main.cpp
@@ -0,0 +1,38 @@
+/*
+   Autores: Caua Balzaneli, Valentino Hoehne
+   Data: 21/05/2026
+   Finalidade:  Ler 20 números em um vetor.
+                Exibir o índice do primeiro número negativo.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    //Criacao do vetor(array)
+    int arr[20];
+
+
+    //Laço de repetição para ler os 20 numeros
+    for(int i = 0; i < 20; i++){
+        cout << "Digite o numero " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    //Laço de repetição para achar o primeiro numero negativo
+    for(int i = 0; i < 20; i++){
+        int numero = arr[i];
+        if(numero < 0){
+            cout << "O indice do primeiro numero negativo eh: " << i;
+        }
+    }
+
+
+
+
+
+    return 0;
+}
